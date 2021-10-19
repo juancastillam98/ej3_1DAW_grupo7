073bafacd9ca294f8a12c5422dca04d174c8d3d3
juancastillam98 <89079551+juancastillam98@users.noreply.github.com>
2021-10-19 17:18:06 +0200

    he escrito hola nacho

--- ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
+++ ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
@@ -5,6 +5,7 @@ public class PruebaG7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola pepe");
+		System.out.println("Hola nacho");
 	}
 
 }
