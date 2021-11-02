a1fd262ee7131a45975fc18cb453c2212e73085b
juancastillam98 <89079551+juancastillam98@users.noreply.github.com>
2021-11-02 16:38:14 +0100

    no me convnence

--- ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
+++ ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
@@ -34,6 +34,7 @@ public class PruebaG7 {
 		System.out.println("Entonces eres más de rock");
 		
 		System.out.println("Ahora se ha puesto de moda el rap Argentino");
+		System.out.println("A mi no me convence");
 
 		}
 
