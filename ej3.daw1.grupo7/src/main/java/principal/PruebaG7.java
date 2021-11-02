97cc3205557f9493b7a0b5f864240216d75fcb1c
juancastillam98 <89079551+juancastillam98@users.noreply.github.com>
2021-11-02 16:40:22 +0100

    ordenado

--- ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
+++ ej3.daw1.grupo7/src/main/java/principal/PruebaG7.java
@@ -7,32 +7,23 @@ public class PruebaG7 {
 		System.out.println("Hola pepe");
 		System.out.println("Hola nacho");
 		System.out.println("pasaba por su casa");
-	
-	
 		System.out.println("por su casa yo pasé");
-
 		System.out.println("yo vi a mi abuela");
 		System.out.println("yo vi a mi vecina");
 		System.out.println("no estaba linda");
 		System.out.println("se puso linda");
 		System.out.println("avisame la proxima vez, para verla yo tambien");
-
 		System.out.println("a ti no te aviso");
-
 		System.out.println("Tienes que entendel");
 		System.out.println("Que a veces no entiendo");
 		System.out.println("Es que es muy feo el regueton");
 		System.out.println("La electronica mola mas");
 		System.out.println("El EDM es la clave");
 		System.out.println("Yo soy más de música clásica");
-
 		System.out.println("Aunque también me gusta el jazz");
-
 		System.out.println("The Beatles estan muy bien");
-		System.out.println("To solo he escchado una canci�n de los beatles");
-			
+		System.out.println("To solo he escchado una canci�n de los beatles");	
 		System.out.println("Entonces eres más de rock");
-		
 		System.out.println("Ahora se ha puesto de moda el rap Argentino");
 		System.out.println("A mi no me convence");
 
